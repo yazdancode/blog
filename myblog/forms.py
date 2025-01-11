c1591e177908cd27556a3771b7ea4987e798fa0d
yshabanei <168936995+yshabanei@users.noreply.github.com>
2025-01-10 20:56:07 -0800

    i add new packages to requirements.txt and new style to formspy

--- myblog/forms.py
+++ myblog/forms.py
@@ -7,8 +7,20 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ("title", "title_tag", "author", "body")
         widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "title_tag": forms.TextInput(attrs={"class": "form-control"}),
+            "title": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "color: #1a202c; font-size: 16px; font-weight: 600; margin-bottom: 2rem; padding-bottom: 10px; border-bottom: 2px solid #e2e8f0;",
+                    "placeholder": "Enter the title here",
+                }
+            ),
+            "title_tag": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "color: #303b51; font-size: 16px; font-weight: 600;",
+                    "placeholder": "Enter the title_tag here"
+
+                }),
             "author": forms.Select(attrs={"class": "form-control"}),
             "body": forms.Textarea(attrs={"class": "form-control"}),
         }
